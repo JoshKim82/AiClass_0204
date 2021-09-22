f3bb6bd3e3c20e89be14b0180b6eba8efe86bd40
Kyoung Dal, Kim <64296073+JoshKim82@users.noreply.github.com>
2021-09-22 11:24:44 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    // 일과 1
+    basic.showNumber(1)
+    basic.pause(600)
+    // 일과 2
+    basic.showNumber(2)
+    basic.pause(100)
+    // 일과 3
+    basic.showNumber(3)
+})
